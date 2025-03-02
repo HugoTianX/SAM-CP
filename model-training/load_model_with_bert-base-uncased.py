0f36de69f17895a0cba3aa8134ea8b025b490241
HugoTianX <tianhao1217@126.com>
2025-03-03 00:03:17 +0800

    Delete model-training directory
    We have reorganized the directory structure to improve clarity and usability. Additionally, the `README.md` file has been updated to provide a better description of the codebase and its functionalities. For details, refer to the updated documentation.

--- model-training/load_model_with_bert-base-uncased.py
+++
@@ -1,7 +0,0 @@
-from transformers import BertModel, BertTokenizer
-
-model = BertModel.from_pretrained('bert-base-uncased')
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-model.save_pretrained('./bert-base-uncased')
-tokenizer.save_pretrained('./bert-base-uncased')
